1035bf97dc11eea0c52e740ab0407307f125d643
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2024-10-11 09:46:16 +0600

    variable, operator

---
+++ let-const.js
@@ -0,0 +1,18 @@
+/* var num1 = 1234;
+console.log(num1);
+var num1 = 456;
+console.log(num1);
+num1 = 789;
+console.log(num1); */
+
+
+/* 
+let num2 = 234;
+console.log(num2);
+
+num2 = 456;
+num2 = 58
+console.log(num2); */
+
+// const num3 = 456;
+
